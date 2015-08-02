1736591b2bc4c4f90e594f289dc9a555d6ae917d
angelkar <angelkar@gmail.com>
2015-08-02 18:30:48 +0300

    Template Method!

--- TemplateMethod/report.rb
+++ TemplateMethod/report.rb
@@ -3,7 +3,8 @@ class Report
 		@title = 'Monthly Report'
 		@text = [ 'Things are going', 'really, really well.']
 	end
-	
+
+	# this is the algorithm, protected by the Report class
 	def output_report
 		output_start
 		output_head
@@ -12,31 +13,30 @@ class Report
 		output_body_end
 		output_end
 	end
-	
+
 	def output_body
 		@text.each do |line|
 			output_line(line)
 		end
 	end
-	
+
 	def output_start
 	end
-	
+
 	def output_head
 		raise 'Called abstract method: output_head'
 	end
-	
+
 	def output_body_start
 	end
-	
+
 	def output_line(line)
 		raise 'Called abstract method: output_line'
 	end
-	
+
 	def output_body_end
 	end
-	
+
 	def output_end
 	end
 end
-
